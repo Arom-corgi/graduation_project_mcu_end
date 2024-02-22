d3896c8156b27e66012095517c9e43fb2447b29a
lzk <1259787536@qq.com>
2024-02-22 17:35:11 +0800

    更新

--- wifi.py
+++ wifi.py
@@ -1,10 +1,11 @@
+import network
+from config import wifi_config
 def do_connect():
-    import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.connect('动手康1', 'dsk20221116')
+        wlan.connect(wifi_config['name'], wifi_config['password'])
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
